3822df031c0c6556862f498f2d2188c45805d54e
saxatachi <saxatachi@gmail.com>
2018-12-04 21:54:40 +0100

    xd

---
+++ Mysite/Mysite/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\saxat\OneDrive\Dokumenty\GitHub\django\Mysite\Mysite\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
